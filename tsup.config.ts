e46381bf38bc12655b0a58ab7e75c7122ded5563
Shahrad Elahi <shahradq@gmail.com>
2024-04-21 03:25:45 +0330

    add `netstat` entrance

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ['src/index.ts', 'src/ip/index.ts'],
+  entry: ['src/index.ts', 'src/ip/index.ts', 'src/netstat/index.ts'],
   format: ['cjs', 'esm'],
   target: 'esnext',
   outDir: 'dist',
